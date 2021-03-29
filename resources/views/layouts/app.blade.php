280d6c38d3c298a4d653c64ec090365b3da1a1ec
alifma <alifmaulana26@gmail.com>
2021-03-29 19:09:54 +0700

    Base Template and Add CSS

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Statusin</title>
+</head>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex items-center">
+            <li>
+                <a href="#" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Status</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li>
+                <a href="#" class="p-3">Alif Maulana A</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="#" class="p-3">Logout</a>
+            </li>
+        </ul>
+    </nav>
+    @yield('content')
+</body>
+</html>
